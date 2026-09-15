d6d359846cd57e688e582d814891080ec58127fd
Viv <viv@users.noreply.github.com>
2026-09-14 22:22:46 -0500

    Remove diagnostic ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -79,18 +79,14 @@ function AnimatedRoutes() {
   );
 }
 
-import ErrorBoundary from './components/ErrorBoundary';
-
 function App() {
   return (
-    <ErrorBoundary>
-      <Router>
-        <Navbar />
-        <main className="w-full relative z-0">
-          <AnimatedRoutes />
-        </main>
-      </Router>
-    </ErrorBoundary>
+    <Router>
+      <Navbar />
+      <main className="w-full relative z-0">
+        <AnimatedRoutes />
+      </main>
+    </Router>
   );
 }
 
