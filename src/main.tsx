a9d7baacb671747a844826077b87821f7160aadf
Viv <viv@users.noreply.github.com>
2026-09-14 22:15:41 -0500

    Inject polyfills to fix fatal pdf.js compatibility crashes on older iOS Safari versions

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,39 @@
+// Polyfills for older Safari versions (iOS 14-16) to support modern pdfjs-dist
+if (!Map.prototype.getOrInsertComputed) {
+  (Map.prototype as any).getOrInsertComputed = function (key: any, callback: (key: any) => any) {
+    if (this.has(key)) return this.get(key);
+    const value = callback(key);
+    this.set(key, value);
+    return value;
+  };
+}
+if (!(Promise as any).withResolvers) {
+  (Promise as any).withResolvers = function () {
+    let resolve, reject;
+    const promise = new Promise((res, rej) => {
+      resolve = res;
+      reject = rej;
+    });
+    return { promise, resolve, reject };
+  };
+}
+if (!Array.prototype.at) {
+  Array.prototype.at = function (index) {
+    index = Math.trunc(index) || 0;
+    if (index < 0) index += this.length;
+    if (index < 0 || index >= this.length) return undefined;
+    return this[index];
+  };
+}
+if (!String.prototype.at) {
+  String.prototype.at = function (index) {
+    index = Math.trunc(index) || 0;
+    if (index < 0) index += this.length;
+    if (index < 0 || index >= this.length) return undefined;
+    return String(this).charAt(index);
+  };
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
